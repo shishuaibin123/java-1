6834c9d6663b4d3e36d9f2f9ef1596af0467b756
石帅斌 <72658884+shishuaibin123@users.noreply.github.com>
2020-10-14 21:48:58 +0800

    Update CPU.java

--- src/shi/CPU.java
+++ src/shi/CPU.java
@@ -2,11 +2,21 @@ package shi;
 
 public class CPU {
   int speed;
+  String brand;
   int getSpeed(){
 	return speed;
   }
-  public void setspeed(int speed){
+	String getBrand()
+	{
+		return brand;
+	}
+	int setSpeed(){
+	      
+	}
+	
+	public void setspeed(int speed,String name){
 	  this.speed=speed;
+	  this.brand=brand;
   }
   
 }
