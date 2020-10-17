916bac434d2cbfcdb1e0ab0d71d4f0a7a407e2ca
石帅斌 <72658884+shishuaibin123@users.noreply.github.com>
2020-10-17 11:38:56 +0800

    Update PC.java

--- src/shi/PC.java
+++ src/shi/PC.java
@@ -12,6 +12,7 @@ public class PC {
   void show(){
 	  System.out.println("CPU:"+cpu.getSpeed());
 	    System.out.println("硬盘容量:"+HD.getAmount());
+	     System.out.println("硬盘价格："+HD.getPrice());
 	  System.out.println("CPU的名称："+cpu.getBrand());
 	
 	  
